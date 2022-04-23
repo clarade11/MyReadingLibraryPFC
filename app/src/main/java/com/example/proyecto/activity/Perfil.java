5eaab922141cf06ef2e04916c875a137dbaf6359
clarade11 <62113757+clarade11@users.noreply.github.com>
2022-04-23 15:52:27 +0200

    cambio contraseña y cambio telefono.Funcionan dialogos :)

--- app/src/main/java/com/example/proyecto/activity/Perfil.java
+++ app/src/main/java/com/example/proyecto/activity/Perfil.java
@@ -7,6 +7,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -16,10 +17,12 @@ import android.widget.TextView;
 
 import com.example.proyecto.R;
 import com.example.proyecto.adapter.Validacion;
+import com.example.proyecto.basedatos.DBHelper;
 import com.example.proyecto.clasesObjeto.Usuario;
 
 public class Perfil extends AppCompatActivity {
 
+    DBHelper DB;
     Usuario usuario;
     ImageView imagePerfil;
     TextView tvPerfilUsuario;
@@ -28,13 +31,15 @@ public class Perfil extends AppCompatActivity {
     TextView cambiarTelefono;
     TextView tvPerfilTelefono;
 
-    Validacion validacion;
+    String telefono;
+    String ps1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-
+        DB = new DBHelper(Perfil.this);
         asociacion();
 
         colocarDatos();
@@ -47,24 +52,55 @@ public class Perfil extends AppCompatActivity {
         cambiarTelefono.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //anuncio en el que poder escribir
+                alertCambioTelefono();
             }
         });
         cambiarContrasena.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Perfil.this, CambiarContrasena.class); //clase nuestra,clase a la que viajar
-                startActivity(i);
+//                Intent i = new Intent(Perfil.this, CambiarContrasena.class); //clase nuestra,clase a la que viajar
+//                startActivity(i);
+
+                alertCambioContrasena();
             }
         });
     }
+
+    private void alertCambioTelefono() {
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(Perfil.this);
+        dialogo.setTitle("Cambio de teléfono");
+
+        final EditText telefonoNuevo = new EditText(Perfil.this);
+        telefonoNuevo.setInputType(InputType.TYPE_CLASS_TEXT);
+        dialogo.setView(telefonoNuevo);
+
+        dialogo.setPositiveButton(R.string.actualizar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                telefono = telefonoNuevo.getText().toString().trim();
+                usuario.setTelefono(telefono);
+                DB.actualizarUsuario(usuario);
+            }
+        });
+
+        dialogo.setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        dialogo.show();
+
+
+    }
+
 
     //ponemos los datos en pantalla
     private void colocarDatos() {
         usuario = MainActivity.usuarioObjeto;
         imagePerfil.setImageResource(R.drawable.negro);
         tvPerfilUsuario.setText(usuario.getUsuario());
-        tvPerfilNombreApellido.setText(usuario.getNombre()+" "+usuario.getApellidos());
+        tvPerfilNombreApellido.setText(usuario.getNombre() + " " + usuario.getApellidos());
         tvPerfilTelefono.setText(usuario.getTelefono());
 
     }
@@ -78,6 +114,35 @@ public class Perfil extends AppCompatActivity {
         cambiarContrasena = (TextView) findViewById(R.id.cambiarContrasena);
         cambiarTelefono = (TextView) findViewById(R.id.cambiarTelefono);
         tvPerfilTelefono = (TextView) findViewById(R.id.tvPerfilTelefono);
+    }
+
+    private void alertCambioContrasena() {
+        AlertDialog.Builder dialogo = new AlertDialog.Builder(Perfil.this);
+        dialogo.setTitle("Cambio de conrtaseña");
+
+        final EditText password1 = new EditText(Perfil.this);
+        password1.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        dialogo.setView(password1);
+
+        dialogo.setPositiveButton(R.string.actualizar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                ps1 = password1.getText().toString().trim();
+
+                usuario.setContrasena(ps1);
+                DB.actualizarUsuario(usuario);
+
+            }
+        });
+
+        dialogo.setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        dialogo.show();
+
 
     }
 
