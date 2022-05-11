22a5cad0967cf857f6b2a27530d7d8c3bf21f3a3
clarade11 <62113757+clarade11@users.noreply.github.com>
2022-05-11 16:50:00 +0200

    cambio version bbdd. Pruebas modificación, eliminación realizadas correctas

--- app/src/main/java/com/example/proyecto/basedatos/DBHelper.java
+++ app/src/main/java/com/example/proyecto/basedatos/DBHelper.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
     // Database Version
-    private static final int DATABASE_VERSION = 7;
+    private static final int DATABASE_VERSION = 8;
 
     // Database Name
     private static final String DATABASE_NAME = "MyReadingDiary.db";
